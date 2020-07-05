e0bcf616708adbd610fc34b626e72c90680144c0
Aiyush <jain.aiyush@fitster.ca>
2020-07-05 00:03:05 -0400

    added icon toggle

--- js/highlight.js
+++ js/highlight.js
@@ -1,5 +1,24 @@
+let toggle = false;
+const icons = [
+    "../images/highlighter-logo-96.png",
+    "../images/highlighter-logo-gs-96.png"
+];
+
 chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.sync.set({color: '#3aa757'}, function() {
         console.log("The color is green.");
+    });
+});
+
+chrome.browserAction.onClicked.addListener(() => {
+    toggle = !toggle;
+    let path;
+    if (toggle) {
+        path = icons[0];
+    } else {
+        path = icons[1];
+    }
+    chrome.browserAction.setIcon({
+        path: path
     });
 });
